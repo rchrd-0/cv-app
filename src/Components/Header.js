074eed359ca4846db90e5913267481a736621ed8
richard <richard.ytli+f@gmail.com>
2022-09-10 17:18:59 +0800

    Refactor Header for function component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,47 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import styled from 'styled-components';
 
-class Header extends Component {
-  render() {
-    const { onClick, mode } = this.props;
-    return (
-      <header>
-        <div id="header-left">
-          <h1>CV App</h1>
-          <div id="made-by">
-            Made by
-            <a
-              className="accent"
-              href="https://github.com/rchrd-0"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img alt="github icon" className="github" />
-              RCHRD
-            </a>
-            for
-            <a
-              className="accent"
-              href="https://www.theodinproject.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              The Odin Project
-            </a>
-          </div>
-        </div>
-        <div id="header-right">
-          <button
-            id="show-hide-btn"
-            type="button"
-            onClick={onClick}
-            className={`${mode ? 'hidden' : 'active'}`}
+const Header = (props) => {
+  const { onClick, mode } = props;
+  return (
+    <header>
+      <div id="header-left">
+        <h1>CV App</h1>
+        <div id="made-by">
+          Made by
+          <a
+            className="accent"
+            href="https://github.com/rchrd-0"
+            target="_blank"
+            rel="noreferrer"
           >
-            {mode ? 'Show form' : 'Hide form'}
-          </button>
+            <img alt="github icon" className="github" />
+            RCHRD
+          </a>
+          for
+          <a
+            className="accent"
+            href="https://www.theodinproject.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            The Odin Project
+          </a>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+      <div id="header-right">
+        <button
+          id="show-hide-btn"
+          type="button"
+          onClick={onClick}
+          className={`${mode ? 'hidden' : 'active'}`}
+        >
+          {mode ? 'Show form' : 'Hide form'}
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
+
+// class Header extends Component {
+//   render() {
+//     const { onClick, mode } = this.props;
+//     return (
+//       <header>
+//         <div id="header-left">
+//           <h1>CV App</h1>
+//           <div id="made-by">
+//             Made by
+//             <a
+//               className="accent"
+//               href="https://github.com/rchrd-0"
+//               target="_blank"
+//               rel="noreferrer"
+//             >
+//               <img alt="github icon" className="github" />
+//               RCHRD
+//             </a>
+//             for
+//             <a
+//               className="accent"
+//               href="https://www.theodinproject.com/"
+//               target="_blank"
+//               rel="noreferrer"
+//             >
+//               The Odin Project
+//             </a>
+//           </div>
+//         </div>
+//         <div id="header-right">
+//           <button
+//             id="show-hide-btn"
+//             type="button"
+//             onClick={onClick}
+//             className={`${mode ? 'hidden' : 'active'}`}
+//           >
+//             {mode ? 'Show form' : 'Hide form'}
+//           </button>
+//         </div>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
